9d5e558b5ad550cceb8971ab123733f95e35f2d3
Michal1898 <michal.volf@gmail.com>
2025-07-14 14:59:13 +0200

    word quessing works.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static String string;
+
+    public static void main(String[] args) {
+        final String Words[] = {"skillmea", "Java", "Python", "Stress", "skull", "Anaconda",
+                "paradies", "hell", "devil", "angel", "holiday", "hardware", "office"};
+        boolean newGame = false;
+        final int Attempts = 6;
+        String guessedWord = "";
+        String secretWord = "";
+        do {
+            //init game
+            guessedWord = "";
+            secretWord = Words[random.nextInt(Words.length)];
+            secretWord = secretWord.toUpperCase();
+            for (int indexOfLetter = 0; indexOfLetter < secretWord.length(); indexOfLetter++) {
+                guessedWord += "-";
+            }
+
+            System.out.println("Hangman");
+            System.out.println("vytvoril Michal Volf");
+            Random random = new Random();
+
+            System.out.println(secretWord);
+            System.out.println(guessedWord);
+            Scanner scanner = new Scanner(System.in);
+            newGame = anotherGame();
+            System.out.println(newGame);
+        } while (newGame);
+        System.out.println("Konec hry");
+    }
+
+    public static Boolean anotherGame() {
+        Scanner again = new Scanner(System.in);
+        String yesNo;
+        while (true) {
+            try {
+                System.out.println("Nova hra?");
+                yesNo = again.nextLine().toLowerCase();
+                if ((yesNo.equals("ano")) || (yesNo.equals("ne"))) {
+                    break;
+                } else {
+                    System.out.println("Odpovez ano, nebo ne!");
+                    again.nextLine();
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+                System.out.println("Odpovez ano, nebo ne!");
+            }
+
+        }
+        if (yesNo.equals("ano")) {
+            System.out.println("Pravda");
+            return true;
+        } else {
+            System.out.println("NePravda");
+            return false;
         }
     }
+
+
 }
+
